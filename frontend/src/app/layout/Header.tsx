c3107bce9c1a9bef90bbb8875c5c2db375f4caf4
Lekkelmendi7 <lek.kelmendi8.com>
2023-12-10 05:01:54 +0100

    Light/Dark Theme Implemented

--- frontend/src/app/layout/Header.tsx
+++ frontend/src/app/layout/Header.tsx
@@ -1,13 +1,19 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+
+interface Props{ 
+    darkMode: boolean;
+    handleThemeChange: () => void;
+}
 
 
-export default function Header(){
+export default function Header({handleThemeChange, darkMode}: Props){
     return(
         <AppBar position='static' sx={{mb: 4, bgcolor:"olive"}}>
             <Toolbar>
                 <Typography>
-                    CAMOUFLAGEE
+                    CAMOUFLAGE
                 </Typography>
+                <Switch checked={darkMode} onChange={handleThemeChange}/>
             </Toolbar>
         </AppBar>
     )
