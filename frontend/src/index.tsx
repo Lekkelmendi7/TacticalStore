407582c84ffc2b86f1c9767aa856c0328c112210
Lekkelmendi7 <lek.kelmendi8.com>
2023-12-11 02:16:39 +0100

    Creating Routes in React

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,9 +6,15 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faStar} from '@fortawesome/free-solid-svg-icons';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/router/Routes.tsx';
+
+library.add(faStar);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
